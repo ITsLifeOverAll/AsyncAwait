a4f5385366e3c45bd663cbf5fd53eb8e9cf2dd4b
Art Tsai <arttsai@gmail.com>
2023-12-01 18:50:10 +0800

    async and threads

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int threadCount = ThreadPool.ThreadCount;
+Console.WriteLine($"Thread Pool thread count: {threadCount}");
+
+var thread = Thread.CurrentThread;
+Console.WriteLine($"Main thread Id: {thread.ManagedThreadId}");
+
+_ = await ExecuteAsync(1);
+
+threadCount = ThreadPool.ThreadCount;
+Console.WriteLine($"Thread Pool thread count: {threadCount}");
+return;
+
+// end of program
+
+async Task<int> ExecuteAsync(int num)
+{
+    // await Task.Delay(500);
+    var result = await Task.FromResult(num);
+    var currentThread = Thread.CurrentThread;
+    Console.WriteLine($"{num,3} Thread Id: {currentThread.ManagedThreadId}");
+    return result;
+}
