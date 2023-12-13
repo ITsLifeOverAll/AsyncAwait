2c1d83deab255060518850ad70d4d483e56fccec
Art Tsai <arttsai@gmail.com>
2023-12-13 11:03:54 +0800

    project: 4WebApi

--- AsyncBreakfast/Program.cs
+++ AsyncBreakfast/Program.cs
@@ -10,13 +10,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int workerThreadCount = 0;
-        int completionThreadCount = 0;
-
-        ThreadPool.GetAvailableThreads(out workerThreadCount, out completionThreadCount);
-        Console.WriteLine($"work thread count: {workerThreadCount}, " +
-            $"completion thread count: {completionThreadCount}");
-
         var stopwatch = Stopwatch.StartNew();
 
         Coffee cup = PourCoffee();                  // 0 secs.
@@ -27,10 +20,6 @@ internal class Program
         Console.WriteLine("eggs are ready");
         Console.WriteLine();
 
-        Bacon bacon = FryBacon(3);                  // 6 secs.
-        Console.WriteLine("bacon is ready");
-        Console.WriteLine();
-
         Toast toast = ToastBread(2);                // 3 secs.
         Console.WriteLine("toast is ready");
         Console.WriteLine();
@@ -38,10 +27,6 @@ internal class Program
         Console.WriteLine("Breakfast is ready!");
         
         Console.WriteLine($"煮早餐耗時: {stopwatch.ElapsedMilliseconds/1000:N} 秒");
-
-        ThreadPool.GetAvailableThreads(out workerThreadCount, out completionThreadCount);
-        Console.WriteLine($"work thread count: {workerThreadCount}, " +
-            $"completion thread count: {completionThreadCount}"); Console.WriteLine();
     }
 
     private static Toast ToastBread(int slices)
@@ -56,24 +41,6 @@ internal class Program
         Console.WriteLine("Remove toast from toaster");
 
         return new Toast();
-    }
-
-    private static Bacon FryBacon(int slices)
-    {
-        Console.WriteLine($"putting {slices} slices of bacon in the pan");
-        Console.WriteLine("cooking first side of bacon...");
-        
-        Task.Delay(3000).Wait();
-        for (int slice = 0; slice < slices; slice++)
-        {
-            Console.WriteLine("flipping a slice of bacon");
-        }
-        Console.WriteLine("cooking the second side of bacon...");
-        
-        Task.Delay(3000).Wait();
-        Console.WriteLine("Put bacon on plate");
-
-        return new Bacon();
     }
 
     private static Egg FryEggs(int howMany)
